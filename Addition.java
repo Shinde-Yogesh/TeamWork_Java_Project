585459cb07c263758c6349c4fde5b3e774839007
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-11-13 15:51:55 +0530

    add the readme file with java code

---
+++ Addition.java
@@ -0,0 +1,13 @@
+package TeamWork_Java_Project;
+
+public class Addition{
+    public static void main(String[] args) {
+        System.out.println("Helllo world");
+        sum(10, 30);
+    }
+
+    public static void sum(int a , int b)
+    {
+        System.out.println(a+b);
+    }
+}
