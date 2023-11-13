cbcfcda9061a7f98203406cfb8dc2bb803783c71
Amardeep Salunke <salunkeamar251@gmail.com>
2023-11-13 18:25:30 +0530

    modified the add class

--- Addition.java
+++ Addition.java
@@ -2,7 +2,7 @@ package TeamWork_Java_Project;
 
 public class Addition{
     public static void main(String[] args) {
-        System.out.println("Helllo world");
+    
         System.out.println(sum(10, 30));
     }
 
