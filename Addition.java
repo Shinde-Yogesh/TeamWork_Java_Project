9f67f2aae44eaf22d0d29bbe9d5036d239d6c6d6
Yogesh Shinde <yogeshshinde2324@gmail.com>
2024-02-07 20:19:21 +0530

    Update Addition.java
    Remove white spaces

--- Addition.java
+++ Addition.java
@@ -2,10 +2,8 @@ package TeamWork_Java_Project;
 
 public class Addition{
     public static void main(String[] args) {
-    
         System.out.println(sum(10, 30));
     }
-
     public static int sum(int a , int b)
     {
        return a + b;
